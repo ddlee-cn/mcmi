a4aa38aa21afc3cbe58065ece933b0eb12ea7873
ddlee96 <cnlijiacheng96@gmail.com>
2017-04-04 16:02:15 +0800

    split test files

--- tests/testthat/test_check.R
+++ tests/testthat/test_check.R
@@ -21,13 +21,4 @@ test_that("test check_integrand",
     expect_error(check_integrand(c(1,2)))
 })
 
-test_that("verify sampling dimensions",
-{
-    test_2_bounds = c(1,2,3,4)
-    test_3_bounds = c(1,2,3,4,5,6)
-    test_n = 100
 
-    expect_equal(dim(sampling(test_2_bounds, test_n))[1], 2)
-    expect_equal(dim(sampling(test_2_bounds, test_n))[2], test_n)
-    expect_equal(dim(sampling(test_3_bounds, test_n))[1], 3)
-})

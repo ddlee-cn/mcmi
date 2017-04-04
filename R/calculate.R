f8da2e431f39fa98311a3bb240309520c6a15fc2
ddlee96 <cnlijiacheng96@gmail.com>
2017-04-04 16:52:10 +0800

    calculation wrapper completed, test passed

--- R/calculate.R
+++ R/calculate.R
@@ -1,1 +1,26 @@
-#
+# Calculate integrations
+# INPUT:
+# rands: random points as a matrix
+# with dim rows and n cloumns
+# integrand: the integrand expression
+# constrains: inequalities which variables should satisfy
+calculate <- function(rands, area, integrand, constrains)
+{
+    n <- dim(rands)[2]
+    dim <- dim(rands)[1]
+    x <- rands[1,]
+    if (dim>1){y <- rands[2,]}
+    if (dim>2){z <- rands[3,]}
+
+    # eval constrains
+    c_num <- length(constrains)
+    constrains_eval = rep(1, n)
+    for (i in 1:c_num){
+        constrains_eval = constrains_eval*eval(parse(text=constrains[i]))
+    }
+
+    return(area * sum(eval(integrand)*constrains_eval)/n)
+
+
+
+}
--- tests/testthat/test_calculate.R
+++ tests/testthat/test_calculate.R
@@ -11,3 +11,17 @@ test_that("verify sampling dimensions",
     expect_equal(dim(sampling(test_2_bounds, test_n))[2], test_n)
     expect_equal(dim(sampling(test_3_bounds, test_n))[1], 3)
 })
+
+test_that("test area calculation",
+{
+    test_1_bounds = c(1,2)
+    test_2_bounds = c(1,2,3,4)
+    test_3_bounds = c(1,2,3,4,5,6)
+
+})
+
+test_that("test ingration calculation",
+{
+    test_rands=matrix(c(runif(10, 1, 2), runif(10, 2, 3), runif(10, 3, 4)), nrow=3, byrow=TRUE)
+    expect_equal(calculate(test_rands, 1, 1, 1), 1)
+})

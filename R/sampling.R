649335fc45dfc7e5858777bd6c397d19c746dbf2
ddlee96 <cnlijiacheng96@gmail.com>
2017-04-04 14:57:28 +0800

    complete sampling function

---
+++ R/sampling.R
@@ -0,0 +1,17 @@
+# generate random points
+# INPUT
+# bounds, dim, n points
+sampling <- function(bounds, n)
+{
+    if(missing(n)){
+        n = 1000000
+    }
+    dim = checkdim(bounds)
+    rand = c()
+    i = 0
+    while(dim>i){
+        rand = c(rand, runif(n, min=bounds[2*i+1], max=bounds[2*(i+1)]))
+        i = i + 1
+    }
+    return(matrix(rand, nrow=dim, byrow=TRUE))
+}

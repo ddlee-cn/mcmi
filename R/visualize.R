42199854c223b253c2277fcbabef393d5daff10a
ddlee96 <cnlijiacheng96@gmail.com>
2017-04-07 14:47:48 +0800

    finally tuned number of random points

--- R/visualize.R
+++ R/visualize.R
@@ -5,7 +5,7 @@
 #' @param bounds:necessary, the bounds of x, y, z, as a vector of 2, 4 or 6 dimensions
 #' @param constrains: optional, inequalities that x, y, z must satisfy, input as character, must contains x, y, or z, default: 1
 #' @examples
-#' visualize_area(c(0,1, 1,2, -1,3), c("exp(x)<y", "sin(z)*y>=0"))
+#' visualize_area(c(-2,2, -2,2, -1,1), c("x^2/4+y^2/4+z^2/1<1", "x<y^2"))
 #'
 #' @export
 visualize_area <- function(bounds, constrains)
@@ -14,7 +14,9 @@ visualize_area <- function(bounds, constrains)
     dim <- check_dim(bounds)
     constrains <- check_constrains(constrains)
 
-    n = 5000
+    area <- calculate_area(bounds)
+
+    n = 500*area
     rands <- sampling(bounds, n)
     x <- rands[1,]
     if (dim>1){y <- rands[2,]}
@@ -28,11 +30,11 @@ visualize_area <- function(bounds, constrains)
     }
 
     if (dim==2){
-        p <- plotly::plot_ly(x=x,y=y,color=constrains_eval,marker=list(opacity=0.2,size=3),colors=c("#0083FF","#FF0000"))
+        p <- plotly::plot_ly(x=x,y=y,color=constrains_eval,marker=list(opacity=0.2,size=2),colors=c("#0083FF","#FF0000"))
         p <- plotly::add_markers(p)
     }
     else if (dim==3){
-        p <- plotly::plot_ly(x=x,y=y,z=z,color=constrains_eval,marker=list(opacity=0.2,size=3),colors=c("#0083FF","#FF0000"))
+        p <- plotly::plot_ly(x=x,y=y,z=z,color=constrains_eval,marker=list(opacity=0.2,size=2),colors=c("#0083FF","#FF0000"))
         p <- plotly::add_markers(p)
     }
     else {stop("only 2D/3D supported.")}
